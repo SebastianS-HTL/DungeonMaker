5b2486288c24e0fec6e3df300d44e862b06195d1
Sebastian Schneiderbauer <146381631+SebastianS-HTL@users.noreply.github.com>
2025-01-24 16:05:40 +0100

    set up project

---
+++ Dungeonmaker/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Dungeonmaker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
